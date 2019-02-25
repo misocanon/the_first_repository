4a5e9a92c2e829567c16bb224298bcca18b616a3
SURFACEPRO3\ahiru_000 <ahirunum@gmail.com>
2019-02-26 03:17:05 +0900

    足し算をするメソッドを追加しました。

--- Sample.java
+++ Sample.java
@@ -3,4 +3,8 @@ public class Sample {
     public static final void main( String[] args ) {
         System.out.println( "Test" );
     }
+
+    public int add( int x, int y ) {
+        return x + y;
+    }
 }
