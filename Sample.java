f706d360a5a92bab67e3ffec8ac9933c8a7130c6
SURFACEPRO3\ahiru_000 <ahirunum@gmail.com>
2019-02-26 03:39:33 +0900

    計算処理を実行します。

--- Sample.java
+++ Sample.java
@@ -2,6 +2,12 @@ public class Sample {
 
     public static final void main( String[] args ) {
         System.out.println( "Test" );
+
+        Sample sample = new Sample();
+
+        int result = sample.add( 1, 2 );
+
+        System.out.println( "計算結果: " + result );
     }
 
     public int add( int x, int y ) {
